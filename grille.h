9f6701b31f144fc35a70816a10c418b358be16ef
e1000ouch <emilien.brando@gmail.com>
2025-11-28 17:34:26 +0100

    Changement de quelques commentaires.

--- grille.h
+++ grille.h
@@ -13,9 +13,9 @@
 // Definition des structures utilisees dans le jeu
 // -- Noah
 typedef struct {
-    int numero; // un sorte de booleen, si il est a 0 c une case pas visité et si il est a un numero 
-    // decroissant par rapport auc curseur il est visité
-    int chiffre; //si c'est un chiffre preset donc toute les valeurs 0<x<12 ou 0 si ce n'est pas un chiffre preset
+    int numero; // un sorte de booleen, si = 0, c'est une case pas encore visité, et si = n > 0, 
+    // permet d'identifier le chemin effectué par le curseur dans un ordre croissant.
+    int chiffre; //soit un chiffre preset donc toute les valeurs 0 < x < 12 ou bien 0 si ce n'est pas un chiffre preset.
 } Case;
 
 typedef struct {
@@ -27,8 +27,7 @@ typedef struct {
 typedef struct {
     int x;
     int y;
-    int chiffre_actuel; // permet de savoir a quel chiffre on est 
-    
+    int chiffre_actuel; // permet de savoir a quel chiffre on est  
 } Position;
 
 
@@ -39,11 +38,12 @@ typedef struct {
     const int (*presets)[TAILLE_MAX][TAILLE_MAX];
 } GrillePresetInfo;
 
-
 void afficher_grille(Grille *grille, Position curseur);
-void  initialiser_grille(Grille* grille, int lignes, int colonnes);
+void initialiser_grille(Grille* grille, int lignes, int colonnes);
 void deplacer_curseur(Position *curseur, Grille *grille, char touche);
+
 Position get_pos1();
+
 bool a_gagne(Position *curseur, Grille *grille);
 
 #endif   
